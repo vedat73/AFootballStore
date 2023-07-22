f009ad72dde7595c14eaf9ac2804de222495e1f8
vedat73 <vedat.ozlu@outlook.com>
2023-07-22 10:12:05 +0300

    Initial Commit

---
+++ HelmetWorld/HelmetWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelmetWorldApp.swift
+//  HelmetWorld
+//
+//  Created by Vedat Ozlu on 22.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct HelmetWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
